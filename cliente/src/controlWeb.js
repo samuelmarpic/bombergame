993e58c0125aa84646bacbc4c857209bdc93a291
SAMUEL MARTÍNEZ PICAZO <Samuel.Martinez6@alu.uclm.es>
2019-10-14 14:01:07 +0200

    Primeros pasos con Boostrap

---
+++ cliente/src/controlWeb.js
@@ -0,0 +1,9 @@
+
+function mostrarAgregarUsuario(){
+    var cadena="<div id='mAU'>";
+    cadena=cadena+"<h3>Usuario</h3>";
+    cadena=cadena+'<input id="nombre" type="text" class="form-control" name="nombre" placeholder="Nombre usuario">';        
+    cadena=cadena+'<button type="button" id="inicioBtn" class="btn btn-primary btn-md">Iniciar Usuario</button>';    
+    cadena=cadena+"</div>";
+
+}
